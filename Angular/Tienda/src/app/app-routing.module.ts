6e18a225f1f093e6f5350d8a9bde19f4c0039281
Isaac <isaac-cantillo@hotmail.com>
2018-12-05 14:14:00 -0400

    Commit #2

--- Angular/Tienda/src/app/app-routing.module.ts
+++ Angular/Tienda/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ComprasComponent } from './compras/compras.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  { path: 'compras', component: ComprasComponent }
 ];
 
 @NgModule({
--- Angular/Tienda/src/app/app.module.ts
+++ Angular/Tienda/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ComprasComponent } from './compras/compras.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    ComprasComponent
   ],
   imports: [
     BrowserModule,
